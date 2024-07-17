2e4f5d321985d119b43dc5f9b45f05bf5042f72c
VrajDalal <vrajdalal04@gmail.com>
2024-07-17 15:30:34 +0530

    some changes done is master branch

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -12,11 +12,13 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"; //eye icon on password
 
 export default function Admin() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     const router = useRouter();
 
     // useAuth()
@@ -36,10 +38,8 @@ export default function Admin() {
                 body: JSON.stringify({ username, password }),
                 credentials: 'include'
             })
-            console.log(adminLoginResponse);
 
             const adminLoginResult = await adminLoginResponse.json()
-            console.log(adminLoginResult);
             if (adminLoginResult.success) {
                 router.push('/admin/dashboard');
             } else {
@@ -54,19 +54,30 @@ export default function Admin() {
         e.preventDefault();
     };
 
+    const env = {
+        theme: {
+            extends: {
+                screens: {
+                    '4k': '2560px',
+                }
+            }
+        }
+    }
+
     return (
         <>
-            <title>Book Issue Hub Admin</title>
-            <div className="flex flex-col md:flex-row h-full" onContextMenu={handleContextMenu}>
-                {/* For Corousel Images*/}
-                <div className="flex justify-center items-center w-full md:w-1/2">
+            <title>Book Issue Hub</title>
 
+            <div className="flex flex-col md:flex-row min-h-screen overflow-hidden">
+                <div className="flex justify-center items-center w-full md:w-1/2 h-full">
                     <Swiper
                         pagination={{ clickable: false }}
                         autoplay={{ delay: 2500, disableOnInteraction: false }}
+                        navigation={false} // add navigation if needed
                         modules={[Pagination, Navigation, Autoplay]}
-                        className="w-screen h-screen"
-                    >
+                        loop={true}
+                        speed={2000}
+                        className="w-full h-full">
                         <SwiperSlide>
                             <Image src="/login-corousal-1.jpg" alt="Image 1" width={1200} height={675} className="object-cover w-full h-screen" />
                         </SwiperSlide>
@@ -79,32 +90,38 @@ export default function Admin() {
                     </Swiper>
                 </div>
 
-                {/*Login Form*/}
-                <div className="flex justify-center items-center w-full md:-w-1/2 bg-[#F8F4EF]">
-                    <div className="flex flex-col items-center">
-                        <div className="mb-6">
-                            <Image src="/logo.jpeg" alt="Logo" width={250} height={250} className="rounded-full" />
+                {/* Login Form */}
+                <div className="flex justify-center items-center h-screen w-full md:w-1/2 bg-[#F8F4EF]">
+                    <div className="flex flex-col items-center w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-3/4 4k:w-3/4 p-6">
+                        <div>
+                            <Image src="/logo.jpeg" alt="Logo" width={200} height={200} className="rounded-full" />
                         </div>
-                        <div className=" rounded-lg w-full sm:max-w-xs md:max-w-sm lg:max-w-lg ">
-                            <div className="flex justify-center items-center">
-                                <h1 className="text-2xl sm:text-3xl md:text-2xl lg:text-5xl xl:text-6xl  font-bold">Login</h1>
-                            </div>
-                            <form className="mb-4" onSubmit={handleAdminLogin}>
+                        <h3 className="text-xl sm:text-1xl md:text-2xl lg:text-3xl 4k:text-4xl md:text-center font-serif mb-5">Welcome to Book Issue Hub</h3>
+                        <div className="w-full max-w-sm lg:max-w-md">
+                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 4k:text-7xl text-center font-bold mb-6">Login</h1>
+                            <form className="space-y-6" onSubmit={handleAdminLogin}>
 
                                 {/* Username Section */}
-                                <div className="mb-4">
-                                    <label htmlFor="txtUsername" className="block text-sm sm:text-sm md:text-1xl lg:text-2xl xl:text-3xl text-black mb-2">Username</label>
-                                    <Input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} className="w-full p-2 pl-10 text-lg text-gray-700" />
+                                <div className="mt-4">
+                                    <label htmlFor="txtUsername" className="block text-sm sm:text-lg 4k:text-4xl md:text-xl font-medium text-black mb-2">Username</label>
+                                    <Input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} className="w-full p-2 sm:p-4 text-base sm:text-lg bg-transparent border-0 border-b-2 border-gray-300 text-gray-700" />
                                 </div>
 
                                 {/* Password Section */}
-                                <div className="mb-4">
-                                    <label htmlFor="txtPassword" className="block text-sm sm:text-sm md:text-1xl lg:text-2xl xl:text-3xl text-black mb-2">Password</label>
-                                    <Input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} className="w-full p-2 pl-10 text-lg text-gray-700" />
+                                <div className="mt-4 relative">
+                                    <label htmlFor="txtPassword" className="block text-sm sm:text-lg md:text-xl 4k:text-4xl font-medium text-black mb-2">Password</label>
+                                    <Input type={showPassword ? 'text' : 'password'} name="password" value={password} onChange={e => setPassword(e.target.value)} className="w-full p-2 sm:p-4 text-base sm:text-lg bg-transparent border-0 border-b-2 border-gray-300 text-gray-700" />
+                                    <button type="button"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+                                    >
+                                        {showPassword ? <AiFillEyeInvisible className="hover:cursor-pointer" size={24} /> : <AiFillEye className="hover:cursor-pointer" size={24} />}
+                                    </button>
                                 </div>
 
+                                {/* Login Button */}
                                 <div className="flex justify-center mb-4">
-                                    <Button type="submit" className="bg-zinc-950 hover:bg-zinc-700 text-white font-bold py-2 px-4 w-full rounded">Login</Button>
+                                    <Button type="submit" className="bg-zinc-950 hover:bg-zinc-700 text-white font-bold py-2 sm:py-3 md:py-4 4k:py-8 px-8 sm:px-10 md:px-12 4k:px-16 text-base sm:text-lg md:text-xl 4k:text-4xl w-full rounded">Login</Button>
                                 </div>
 
                             </form>
@@ -112,6 +129,8 @@ export default function Admin() {
                     </div>
                 </div>
             </div>
+
+
         </>
     );
 }
